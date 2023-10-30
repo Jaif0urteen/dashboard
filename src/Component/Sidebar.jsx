8aacf7d1cf8685d567824bce65d34b2910504daf
Jaif0urteen <jp3640890@gmail.com>
2023-10-30 18:43:44 +0530

    five

--- src/Component/Sidebar.jsx
+++ src/Component/Sidebar.jsx
@@ -1,21 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { FaTruckMoving, FaChalkboardTeacher } from 'react-icons/fa'
-// import MasterGST from './MasterGST'
-// import MasterUOM from './MasterUOM'
-// import MasterBusinessType from './MasterBusinessType'
-// import MasterPincode from './MasterPincode'
-// import MasterBrandname from './MasterBrandname'
-// import MasterTypeOfCompany from './MasterTypeOfCompany'
 
 
 export default function Sidebar({ toggle }) {
-  const [selected, setSelected] = useState('GST (%)')
 
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    setSelected(e.target.value)
-  }
 
   return (
     <>
@@ -32,54 +21,54 @@ export default function Sidebar({ toggle }) {
         <ul className="sidebar-nav" id="sidebar-nav">
 
           <li className="nav-item">
-            <Link className="nav-link " to="/admin-basic-info">
+            <Link className="nav-link collapsed" to="/admin-basic-info">
               <i><FaTruckMoving /></i>
               <span>Supplier Management</span>
             </Link>
           </li>
 
+         
+
           <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
-          <i><FaChalkboardTeacher /></i>
-          <span>Master Page</span>
-          <i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <Link to='/admin-master-gst-page'>
-              <i class="bi bi-circle"></i><span>GST (%)</span>
-            </Link>
+            <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+              <i><FaChalkboardTeacher /></i>
+              <span>Master Page</span>
+              <i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+              <li>
+                <Link to='/admin-master-gst-page'>
+                  <i class="bi bi-circle"></i><span>GST (%)</span>
+                </Link>
+              </li>
+              <li>
+                <Link to='/admin-master-uom-page'>
+                  <i class="bi bi-circle"></i><span>UOM</span>
+                </Link>
+              </li>
+              <li>
+                <Link to='/admin-master-business-type-page'>
+                  <i class="bi bi-circle"></i><span>Business Type</span>
+                </Link>
+              </li>
+              <li>
+                <Link to='/admin-master-pincode-page'>
+                  <i class="bi bi-circle"></i><span>Pincode</span>
+                </Link>
+              </li>
+              <li>
+                <Link to='/admin-master-brand-name-page'>
+                  <i class="bi bi-circle"></i><span>Brand Name</span>
+                </Link>
+              </li>
+              <li>
+                <Link to='/admin-master-type-of-company-page'>
+                  <i class="bi bi-circle"></i><span>Type of Company</span>
+                </Link>
+              </li>
+            </ul>
           </li>
-          <li>
-            <Link to='/admin-master-uom-page'>
-              <i class="bi bi-circle"></i><span>UOM</span>
-            </Link>
-          </li>
-          <li>
-            <Link to='/admin-master-business-type-page'>
-              <i class="bi bi-circle"></i><span>Business Type</span>
-            </Link>
-          </li>
-          <li>
-            <Link to='/admin-master-pincode-page'>
-              <i class="bi bi-circle"></i><span>Pincode</span>
-            </Link>
-          </li>
-          <li>
-            <Link to='/admin-master-brand-name-page'>
-              <i class="bi bi-circle"></i><span>Brand Name</span>
-            </Link>
-          </li>
-          <li>
-            <Link to='/admin-master-type-of-company-page'>
-              <i class="bi bi-circle"></i><span>Type of Company</span>
-            </Link>
-          </li>
-        </ul>
-      </li>
-          
-          {/* 
-       <!-- End Dashboard Nav --> */}
+
         </ul>
 
 
